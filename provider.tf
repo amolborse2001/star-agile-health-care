fa706f383a370ccd9d8fb8c83198e6b0cfdba6a4
amolborse2001 <115874482+amolborse2001@users.noreply.github.com>
2024-10-20 20:24:39 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"  # You can specify the version range you want
+    }
+  }
+
+  required_version = ">= 1.0"  # Ensure Terraform version is 1.0 or higher
+}
+
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-1"  # Specify your desired AWS region
 }
