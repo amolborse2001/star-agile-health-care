7bd0d5ee1a384b9f54433fba9f4ea7df6a091df8
amolborse2001 <115874482+amolborse2001@users.noreply.github.com>
2024-10-20 20:32:24 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,12 +1,12 @@
 locals {
-  **vpc_name**           = "your-vpc-name"  # Ensure this matches the eks_name if intended
-  **vpc_cidr**           = "10.0.0.0/16"     # Adjust CIDR block as needed
-  **vpc_availability_zones** = ["us-east-1a", "us-east-1b"]  # Change this name
-  **vpc_public_subnets**     = ["10.0.1.0/24"]   # Change this name
-  **vpc_private_subnets**    = ["10.0.2.0/24"]   # Change this name
-  **vpc_intra_subnets**      = ["10.123.5.0/24", "10.123.6.0/24"]  # Change this name
+  vpc_name           = "your-vpc-name"  # Ensure this matches the eks_name if intended
+  vpc_cidr           = "10.0.0.0/16"     # Adjust CIDR block as needed
+  vpc_availability_zones = ["us-east-1a", "us-east-1b"]  # Change this name
+  vpc_public_subnets     = ["10.0.1.0/24"]   # Change this name
+  vpc_private_subnets    = ["10.0.2.0/24"]   # Change this name
+  vpc_intra_subnets      = ["10.123.5.0/24", "10.123.6.0/24"]  # Change this name
 
-  **vpc_tags** = {  # Change this name
+  vpc_tags = {  # Change this name
     Name        = local.vpc_name
     Environment = "dev"  # Adjust as necessary
   }
