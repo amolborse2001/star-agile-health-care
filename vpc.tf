558851041e78d1f21c434f763112492002085d83
amolborse2001 <115874482+amolborse2001@users.noreply.github.com>
2024-10-20 17:28:08 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,35 +1,13 @@
-locals {
-  name             = "my-vpc"
-  vpc_cidr         = "10.123.0.0/16"
-  azs              = ["us-east-1a", "us-east-1b"]
-  private_subnets  = ["10.123.3.0/24", "10.123.4.0/24"]
-  public_subnets   = ["10.123.1.0/24", "10.123.2.0/24"]
-  intra_subnets    = ["10.123.5.0/24", "10.123.6.0/24"]
-}
-
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
-  version = "~> 4.0"
+  version = "3.14.0"
 
-  name                  = local.name
-  cidr                  = local.vpc_cidr
-  azs                   = local.azs
-  private_subnets       = local.private_subnets
-  public_subnets        = local.public_subnets
-  intra_subnets         = local.intra_subnets
-  enable_nat_gateway     = true
-  map_public_ip_on_launch = true
+  name              = local.vpc_name
+  cidr              = local.vpc_cidr
+  azs               = local.availability_zones
+  public_subnets    = local.public_subnets
+  private_subnets   = local.private_subnets
+  intra_subnets     = local.intra_subnets
 
-  public_subnet_tags = {
-    "kubernetes.io/role/elb" = 1
-  }
-
-  private_subnet_tags = {
-    "kubernetes.io/role/internal-elb" = 1
-  }
-
-  tags = {
-    Name        = local.name
-    Environment = "dev"
-  }
+  tags = local.tags
 }
